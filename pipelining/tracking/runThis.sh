366cafca08f07c245368a3cd769e6a42998b7dcc
Joseph Viviano <jviviano@bulbasaur.camhres.ca>
2017-09-28 18:09:53 -0400

    cleaned up

--- pipelining/tracking/runThis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. $MODULESHOME/init/bash
-module load python
-
-projectName=${projName}
-subjectID=${subjID}
-
-python /scratch/lliu/ConnectivityPipe/connectivityPipeline.py ${projectName} ${subjectID}
--- weighted_mat/connectionWeight.m
+++ weighted_mat/connectionWeight.m
@@ -4,6 +4,7 @@
 % RUN voxelCount.m FIRST!
 
 function output = connectionWeight(dtiList, lengthList)
+
     for dtiIndex = 1:length(dtiList)
         try
             dtiSplit = strsplit(char(dtiList(dtiIndex)), '_');
@@ -19,21 +20,22 @@ function output = connectionWeight(dtiList, lengthList)
             weightedMat = [];
             weightedLength = [];
 
-        for roi1 = 1:268
-            for roi2 = 1:268
-                n_fibers = conMat(roi1, roi2);
-                weightedMat(roi1, roi2) = 2 * n_fibers / (voxelCounts(roi1) + voxelCounts(roi2));
-                if lengthsMat(roi1, roi2) > 0
-                    weightedLength(roi1, roi2) = 2 / (voxelCounts(roi1) + voxelCounts(roi2)) / lengthsMat(roi1, roi2);
+            for roi1 = 1:268
+                for roi2 = 1:268
+                    n_fibers = conMat(roi1, roi2);
+                    weightedMat(roi1, roi2) = 2 * n_fibers / (voxelCounts(roi1) + voxelCounts(roi2));
+
+                    if lengthsMat(roi1, roi2) > 0
+                        weightedLength(roi1, roi2) = 2 / (voxelCounts(roi1) + voxelCounts(roi2)) / lengthsMat(roi1, roi2);
+                    end
                 end
             end
-        end
 
-        weightedStreamName = strcat(char(dtiSubjID), '_bedpostX_det_connectivity_weighted.csv');
-        weightedLengthName = strcat(char(dtiSubjID), '_bedpostX_det_length_weighted.csv');
-        conmatPath = strcat('/scratch/lliu/SPINS/pipelines/conmat/', char(dtiSubjID));
-        csvwrite(fullfile(conmatPath, weightedStreamName), weightedMat, 1, 0);
-     	  csvwrite(fullfile(conmatPath, weightedLengthName), weightedLength, 1, 0);
+            weightedStreamName = strcat(char(dtiSubjID), '_bedpostX_det_connectivity_weighted.csv');
+            weightedLengthName = strcat(char(dtiSubjID), '_bedpostX_det_length_weighted.csv');
+            conmatPath = strcat('/scratch/lliu/SPINS/pipelines/conmat/', char(dtiSubjID));
+            csvwrite(fullfile(conmatPath, weightedStreamName), weightedMat, 1, 0);
+         	csvwrite(fullfile(conmatPath, weightedLengthName), weightedLength, 1, 0);
 
         catch
             continue
