8cafe5868d038f6278f5ae83a47be9b709fb3225
Joseph Viviano <jviviano@kandel.kimel.ric>
2017-11-10 17:07:35 -0500

    first prototype done

--- dti_conn.py
+++ dti_conn.py
@@ -12,101 +12,16 @@ logging.basicConfig(level=logging.WARN, format="[%(name)s] %(levelname)s: %(mess
 logger = logging.getLogger(os.path.basename(__file__))
 #tmpdir = tempfile.mkdtemp(prefix='dti-conn-')
 
-
-def connectivity():
-    os.chdir(tempSubjDir)
-    if not os.path.exists(conmatPipeDir):
-        os.makedirs(conmatPipeDir)
-
-    if os.listdir(conmatPipeDir)==[]:
-        register()
-        os.system('echo "streamlining"')
-        os.system('track \
-            -bedpostxdir ' + tempSubjName + '.bedpostX' + ' \
-            -inputmodel bedpostx_dyad \
-            -seedfile wmparc_invert_bin.nii.gz \
-            -curvethresh 90 \
-            -curveinterval 2.5 \
-            -anisthresh 0.2 \
-            -tracker rk4 \
-            -interpolator linear \
-            -iterations 1 \
-            -brainmask nodif_brain_mask.nii.gz | procstreamlines \
-            -endpointfile atlas.nii.gz \
-            -outputfile bedDetTracts.Bfloat')
-
-        os.system('echo "calculating connectivity matrix"')
-        os.system('conmat \
-            -inputfile bedDetTracts.Bfloat \
-            -targetfile atlas.nii.gz \
-            -tractstat length \
-            -outputroot ' + subjectID + '_bedpostX_det_')
-
-        conExt = '*_bedpostX_det_sc.csv'
-        lenExt = '*_bedpostX_det_ts.csv'
-
-        shutil.copyfile(tempSubjDir + glob('bedDetTracts.Bfloat')[0], conmatPipeDir + subjectID + '_detTracts.Bfloat')
-        shutil.copyfile(tempSubjDir + glob('*.scheme')[0], conmatPipeDir + subjectID + '.scheme')
-        shutil.copyfile(tempSubjDir + glob('atlas.nii.gz')[0], conmatPipeDir + subjectID + '_registered_shen.nii.gz')
-        shutil.copyfile(tempSubjDir + glob(conExt)[0], conmatPipeDir + subjectID + '_bedpostX_det_connectivity.csv')
-        shutil.copyfile(tempSubjDir + glob(lenExt)[0], conmatPipeDir + subjectID + '_bedpostX_det_length.csv')
-
-def get_fa_matrix(stat):
-    os.chdir(tempSubjDir)
-    faCSV = conmatPipeDir + subjectID + '_bedpostX_det_fa_' + stat + '.csv'
-    if not os.path.exists(faCSV):
-        try:
-            faFile = tempSubjDir + glob('*_FA.nii.gz')[0]
-
-            logger.info('calculating connectivity matrix')
-            os.system('conmat \
-                -inputfile bedDetTracts.Bfloat \
-                -targetfile atlas.nii.gz \
-                -scalarfile {} -tractstat {} -outputroot {}_fa_{}_'.format(
-                    faFile, stat, subjectID, stat))
-        except:
-            register()
-            logger.info('calculating FA map')
-            os.system('fa -inputfile wdt.nii.gz -outputfile fa.nii.gz')
-
-            logger.info('calculating connectivity matrix')
-            os.system('conmat \
-                -inputfile bedDetTracts.Bfloat \
-                -targetfile atlas.nii.gz \
-                -scalarfile fa.nii.gz \
-                -tractstat {} -outputroot {}_fa_{}_'.format(stat, subjectID, stat))
-
-    faExt = '*_fa_' + stat + '_ts.csv'
-    shutil.copyfile(tempSubjDir + glob(faExt)[0], faCSV)
-
-
-def get_md_matrix(stat):
-    os.chdir(tempSubjDir)
-    mdCSV = conmatPipeDir + subjectID + '_bedpostX_det_md_' + stat + '.csv'
-    mdFile = tempSubjDir + glob('*_MD.nii.gz')[0]
-    mdExt = '*_md_' + stat + '_ts.csv'
-
-    if not os.path.exists(mdCSV):
-        try:
-            logger.info('calculating connectivity matrix')
-            os.system('conmat \
-                -inputfile bedDetTracts.Bfloat \
-                -targetfile atlas.nii.gz \
-                -scalarfile {} -tractstat {} -outputroot {}_md_{}_ '.format(
-                    mdFile, stat, subjectID, stat))
-
-            shutil.copyfile(tempSubjDir + glob(mdExt)[0], conmatPipeDir + subjectID + '_bedpostX_det_md_' + stat + '.csv')
-        except:
-            register()
-            os.system('md -inputfile wdt.nii.gz -outputfile md.nii.gz')
-
-            os.system('echo "calculating connectivity matrix"')
-            os.system('conmat \
-                -inputfile bedDetTracts.Bfloat \
-                -targetfile atlas.nii.gz \
-                -scalarfile md.nii.gz \
-                -tractstat {} -outputroot {}_md_{}_'.format(stat, subjectID, stat))
-            shutil.copyfile(tempSubjDir + glob(mdExt)[0], conmatPipeDir + subjectID + '_bedpostX_det_md_' + stat + '.csv')
+# eventually could be used to a yaml configuration file or similar
+OPTIONS = {
+    'iterations': 1000,
+    'curvethresh': 90,
+    'curveinterval': 2.5,
+    'anisthresh': 0.2,
+    'tracker': 'rk4',
+    'interpolator': 'linear',
+    'stepsize': 0.5
+}
 
 
 def camino_probabilisitc(dwi, bvec, bval, b0_mask, output_prefix):
@@ -123,12 +38,12 @@ def camino_probabilisitc(dwi, bvec, bval, b0_mask, output_prefix):
         op=output_prefix))
     run('track -inputmodel pico -seedfile {op}_reg_wm_to_b0.nii.gz -iterations 1000 < {op}_modelfit_probabilistic_pdfs.Bdouble > {op}_modelfit_probabilistic_tracts.Bfloat'.format(
         op=output_prefix))
-    run('conmat -inputfile {op}_modelfit_probabilistic_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -tractstat length -outputroot {op}_conmat_prob_'.format(
-        op=output_prefix))
     run('cat {op}_modelfit_probabilistic.Bdouble | fa | voxel2image -outputroot {op}_modelfit_probabilistic_fa -header {dwi}'.format(
         dwi=dwi, op=output_prefix))
     run('cat {op}_modelfit_probabilistic.Bdouble | md | voxel2image -outputroot {op}_modelfit_probabilistic_md -header {dwi}'.format(
         dwi=dwi, op=output_prefix))
+    run('conmat -inputfile {op}_modelfit_probabilistic_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -tractstat length -outputroot {op}_conmat_prob_'.format(
+        op=output_prefix))
 
 
 def camino_deterministic(dwi, bvec, bval, b0_mask, output_prefix):
@@ -137,14 +52,42 @@ def camino_deterministic(dwi, bvec, bval, b0_mask, output_prefix):
         bvec=bvec, bval=bval, op=output_prefix))
     run('wdtfit {dwi} {op}_bvec.scheme -bgmask {b0_mask} -outputfile {op}_wdfit_determinisitc.nii.gz'.format(
         dwi=dwi, b0_mask=b0_mask, op=output_prefix))
-    run('track -inputfile {op}_wdfit_determinisitc.nii.gz -inputmodel dt -seedfile {op}_reg_wm_to_b0.nii.gz -curvethresh 90 -curveinterval 2.5 -anisthresh 0.2 -tracker rk4 -interpolator linear -stepsize 0.5 -iterations 100 -brainmask {b0_mask} | procstreamlines -endpointfile {op}_reg_atlas_to_b0.nii.gz -outputfile {op}_wdfit_determinisitc_tracts.Bfloat'.format(
-        b0_mask=b0_mask, op=output_prefix))
+    run('track -inputfile {op}_wdfit_determinisitc.nii.gz -inputmodel dt -seedfile {op}_reg_wm_to_b0.nii.gz -curvethresh {curvethresh} -curveinterval {curveinterval} -anisthresh {anisthresh} -tracker {tracker} -interpolator {interpolator} -stepsize {stepsize} -iterations {iterations} -brainmask {b0_mask} | procstreamlines -endpointfile {op}_reg_atlas_to_b0.nii.gz -outputfile {op}_wdfit_determinisitc_tracts.Bfloat'.format(
+        b0_mask=b0_mask, op=output_prefix, **OPTIONS))
     run('md -inputfile {op}_wdfit_determinisitc.nii.gz -outputfile {op}_wdfit_determinisitc_md.nii.gz'.format(
         op=output_prefix))
     run('fa -inputfile {op}_wdfit_determinisitc.nii.gz -outputfile {op}_wdfit_determinisitc_fa.nii.gz'.format(
         op=output_prefix))
+
+    # connectivity measure
     run('conmat -inputfile {op}_wdfit_determinisitc_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -tractstat length -outputroot {op}_wdfit_determinisitc_conn_'.format(
         op=output_prefix))
+    # max, mean, min FA and MD stat per connection
+    run('conmat -inputfile {op}_wdfit_determinisitc_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -scalarfile {op}_wdfit_determinisitc_fa.nii.gz -tractstat max -outputroot {op}_wdfit_deterministic_fa_max_'.format(
+        op=output_prefix))
+    run('conmat -inputfile {op}_wdfit_determinisitc_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -scalarfile {op}_wdfit_determinisitc_fa.nii.gz -tractstat mean -outputroot {op}_wdfit_deterministic_fa_mean_'.format(
+        op=output_prefix))
+    run('conmat -inputfile {op}_wdfit_determinisitc_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -scalarfile {op}_wdfit_determinisitc_fa.nii.gz -tractstat min -outputroot {op}_wdfit_deterministic_fa_min_'.format(
+        op=output_prefix))
+    run('conmat -inputfile {op}_wdfit_determinisitc_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -scalarfile {op}_wdfit_determinisitc_md.nii.gz -tractstat max -outputroot {op}_wdfit_deterministic_md_max_'.format(
+        op=output_prefix))
+    run('conmat -inputfile {op}_wdfit_determinisitc_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -scalarfile {op}_wdfit_determinisitc_md.nii.gz -tractstat mean -outputroot {op}_wdfit_deterministic_md_mean_'.format(
+        op=output_prefix))
+    run('conmat -inputfile {op}_wdfit_determinisitc_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -scalarfile {op}_wdfit_determinisitc_md.nii.gz -tractstat min -outputroot {op}_wdfit_deterministic_md_min_'.format(
+        op=output_prefix))
+
+
+def bedpostx(bpost_dir, b0_mask, output_prefix):
+    """computes probabilisitc and deterministic bedpostx outputs"""
+    run('track -bedpostxdir {bpost_dir} -inputmodel bedpostx -seedfile {op}_reg_wm_to_b0.nii.gz -curvethresh {curvethresh} -curveinterval {curveinterval} -anisthresh {anisthresh} -tracker {tracker} -interpolator {interpolator} -iterations {iterations} -brainmask {b0_mask} | procstreamlines -endpointfile {op}_reg_atlas_to_b0.nii.gz -outputfile {op}_bedpost_probabilistic_tracts.Bfloat'.format(
+        bpost_dir=bpost_dir, b0_mask=b0_mask, op=output_prefix, **OPTIONS))
+    run('track -bedpostxdir {bpost_dir} -inputmodel bedpostx_dyad -seedfile {op}_reg_wm_to_b0.nii.gz -curvethresh {curvethresh} -curveinterval {curveinterval} -anisthresh {anisthresh} -tracker {tracker} -interpolator {interpolator} -iterations {iterations} -brainmask {b0_mask} | procstreamlines -endpointfile {op}_reg_atlas_to_b0.nii.gz -outputfile {op}_bedpost_deterministic_tracts.Bfloat'.format(
+        bpost_dir=bpost_dir, b0_mask=b0_mask, op=output_prefix, **OPTIONS))
+    run('conmat -inputfile {op}_bedpost_probabilistic_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -tractstat length -outputroot {op}_bedpost_probabilistic_'.format(
+        op=output_prefix))
+    run('conmat -inputfile {op}_bedpost_deterministic_tracts.Bfloat -targetfile {op}_reg_atlas_to_b0.nii.gz -tractstat length -outputroot {op}_bedpost_deterministic_'.format(
+        op=output_prefix))
+
 
 
 def register(b0, t1, t1_wm, atlas, atlas_anat, output_prefix):
@@ -165,7 +108,7 @@ def register(b0, t1, t1_wm, atlas, atlas_anat, output_prefix):
         os.remove('{op}_reg_wm_to_b0_tmp.nii.gz'.format(op=output_prefix))
 
     # register atlas to b0
-    if not os.path.isfile('{op}_reg_atlas_to_b0.nii.gz'.format(op=output_prefix):
+    if not os.path.isfile('{op}_reg_atlas_to_b0.nii.gz'.format(op=output_prefix)):
         run('flirt -in {t1} -ref {mni} -omat {op}_reg_t1_to_mni.mat -o {op}_reg_t1_to_mni.nii.gz'.format(
             t1=t1, mni=atlas_anat, op=output_prefix))
         run('convert_xfm -omat {op}_reg_mni_to_t1.mat -inverse {op}_reg_t1_to_mni.mat'.format(
@@ -176,18 +119,15 @@ def register(b0, t1, t1_wm, atlas, atlas_anat, output_prefix):
             atlas=atlas, b0=b0, op=output_prefix))
 
 
-def run(cmd, dryrun=False, specialquote=True, verbose=True):
+def run(cmd):
     """runs command in default shell, returning STDOUT and a return code."""
     # perform shell quoting for special characters in filenames
-    if specialquote:
-        cmd = _escape_shell_chars(cmd)
-
-    logger.debug("Executing command: {}".format(cmd))
+    logger.debug("executing command: {}".format(cmd))
 
     p = proc.Popen(cmd, shell=True, stdout=proc.PIPE, stderr=proc.PIPE)
     out, err = p.communicate()
 
-    if p.returncode and verbose:
+    if p.returncode:
         logger.error('command {} failed with code {}.\n STDERR: {}'.format(
             cmd, p.returncode, err))
 
@@ -200,9 +140,14 @@ def check_inputs(args, arglist, directory=False):
     true, then checks for directories instead
     """
     for arg in arglist:
-        if not os.path.isfile(args.__dict__[arg]):
-            logger.error('input {}={} not found'.format(arg, args.__dict__[arg]))
-            sys.exit(1)
+        if directory:
+            if not os.path.isdir(args.__dict__[arg]):
+                logger.error('input directory {}={} not found'.format(arg, args.__dict__[arg]))
+                sys.exit(1)
+        else:
+            if not os.path.isfile(args.__dict__[arg]):
+                logger.error('input file {}={} not found'.format(arg, args.__dict__[arg]))
+                sys.exit(1)
 
 
 if __name__ == '__main__':
@@ -227,6 +172,13 @@ if __name__ == '__main__':
     if args.bedpostx:
         check_inputs(args, ['bedpostx'], directory=True)
 
+    # set debugging
+    logger.info('starting')
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
     # collect bvec and bvals
     if args.dwi.endswith('nii.gz'):
         dwistem = os.path.splitext(os.path.splitext(args.dwi)[0])[0]
@@ -235,18 +187,10 @@ if __name__ == '__main__':
     bvec = '{}.bvec'.format(dwistem)
     bval = '{}.bval'.format(dwistem)
 
-    import IPython; IPython.embed()
-
     register(args.b0, args.t1, args.t1_wm, args.atlas, args.atlas_anat, args.output_prefix)
     camino_deterministic(args.dwi, bvec, bval, args.b0_mask, args.output_prefix)
-    camino_probabilistic(args.dwi, bvec, bval, args.b0_mask, args.output_prefix)
+    camino_probabilisitc(args.dwi, bvec, bval, args.b0_mask, args.output_prefix)
 
-    # put these inside each camino fxn?
-    get_fa_matrix('max')
-    get_fa_matrix('mean')
-    get_fa_matrix('min')
-    get_md_matrix('max')
-    get_md_matrix('mean')
-    get_md_matrix('min')
-
+    if args.bedpostx:
+        bedpostx(args.bedpostx, args.b0_mask, args.output_prefix)
 
--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 PATH=${PATH}:${PWD}
 
-dti_conn.py \
-    ~/dti_test/T1w_brain.nii.gz \
-    ~/dti_test/wmparc.nii.gz \
-    ~/dti_test/shen_2mm_268_parcellation.nii.gz \
-    ~/dti_test/MNI152_T1_2mm_brain.nii.gz \
-    ~/dti_test/SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct_b0_bet.nii.gz \
-    ~/dti_test/SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct_b0_bet_mask.nii.gz \
-    ~/dti_test/SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5.nii.gz \
-    ~/dti_test/dti_conn_test
+dti_conn.py -v \
+    -b ~/dti_conn/bedpostx \
+    ~/dti_conn/T1w_brain.nii.gz \
+    ~/dti_conn/wmparc.nii.gz \
+    ~/dti_conn/shen_2mm_268_parcellation.nii.gz \
+    ~/dti_conn/MNI152_T1_2mm_brain.nii.gz \
+    ~/dti_conn/SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct_b0_bet.nii.gz \
+    ~/dti_conn/SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct_b0_bet_mask.nii.gz \
+    ~/dti_conn/SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5.nii.gz \
+    ~/dti_conn/dti_conn_test
