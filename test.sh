3ff21f89eda5653a6d4d3afcb5227f5df4665cdb
Joseph Viviano <jviviano@kandel.kimel.ric>
2017-11-14 15:27:16 -0500

    added tests

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 PATH=${PATH}:${PWD}
 
 dti_conn.py -v \
-    -b ~/dti_conn/bedpostx \
-    ~/dti_conn/T1w_brain.nii.gz \
-    ~/dti_conn/wmparc.nii.gz \
-    ~/dti_conn/shen_2mm_268_parcellation.nii.gz \
-    ~/dti_conn/MNI152_T1_2mm_brain.nii.gz \
-    ~/dti_conn/SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct_b0_bet.nii.gz \
-    ~/dti_conn/SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct_b0_bet_mask.nii.gz \
-    ~/dti_conn/SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5.nii.gz \
-    ~/dti_conn/dti_conn_test
+    -b dti_conn_test/bedpostx \
+    dti_conn_test/T1w_brain.nii.gz \
+    dti_conn_test/wmparc.nii.gz \
+    dti_conn_test/shen_2mm_268_parcellation.nii.gz \
+    dti_conn_test/MNI152_T1_2mm_brain.nii.gz \
+    dti_conn_test/subject_b0_bet.nii.gz \
+    dti_conn_test/subject_b0_bet_mask.nii.gz \
+    dti_conn_test/subject.nii.gz \
+    dti_conn_test/dti_conn_test
